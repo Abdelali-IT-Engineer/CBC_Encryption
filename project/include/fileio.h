8b70be6b34625549a481d391501bbfb28d66c65b
Abdelali-IT-Engineer <165439618+Abdelali-IT-Engineer@users.noreply.github.com>
2025-01-28 13:13:30 +0100

    Update fileio.h

--- project/include/fileio.h
+++ project/include/fileio.h
@@ -3,11 +3,10 @@
 
 #include "../include/linkedlist.h"
 
-// Fonction pour lire les données d'un fichier et les stocker dans une liste
-// chaînée
+// Fonction pour lire les données d'un fichier et les stocker dans une liste chaînée
 struct Node *readFile(const char *filename);
 
 // Fonction pour écrire les données d'une liste chaînée dans un fichier
 void writeFile(const char *filename, struct Node *data);
 
-#endif // FILEIO_H
+#endif 
